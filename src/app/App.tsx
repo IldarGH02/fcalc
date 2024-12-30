a3ac2e0a44ce1964f89ead190ddb9386a0d9837e
Ильдар Халилов <ildarhalilov@MacBook-Pro-Ildar.local>
2024-12-31 00:12:35 +0400

    fix

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,19 @@
 import { Suspense, useEffect } from "react";
 import { RootRouting } from "../pages";
+import { Navigate, useLocation, useNavigate } from "react-router-dom";
 
 const tg = window.Telegram.WebApp;
 
 const App = () => {
+    const location = useLocation()
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        if(location.pathname.includes('/')) {
+            navigate('/home')
+        }
+    })
+
     useEffect(() => {
         console.log(tg.initDataUnsafe.user)
     }, [])
